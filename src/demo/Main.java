9becc3d7fec650653a7caafe412e03ea98a8c12e
jsrinidhi870-crypto <jsrinidhi870@email.com>
2026-02-09 13:42:00 +0530

    folder created

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -1,4 +1,6 @@
-    //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+package demo;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
--- src/demo/print.java
+++ src/demo/print.java
@@ -1,3 +1,5 @@
+package demo;
+
 public class print {
     public static void main(String[] args){
         System.out.print("Hello World");
